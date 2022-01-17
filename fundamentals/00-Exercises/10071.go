c516216d0d3ca67aa2229c81a622dfcea0b3b624
enricozab <enricozabayle@cobenagroup.com>
2022-01-17 16:49:19 +0800

    added samples

---
+++ fundamentals/00-Exercises/10071.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var v, t int
+	_, err := fmt.Scan(&v, &t)
+
+	for err == nil {
+		fmt.Println(2 * v * t)
+		_, err = fmt.Scan(&v, &t)
+	}
+}
---
+++ fundamentals/sample1.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	println("Hello there! How do you do?")
+}
