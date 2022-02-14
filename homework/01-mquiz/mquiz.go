cef54600bbcb29daee78b65e320501595fb42768
enricozab <enricozabayle@cobenagroup.com>
2022-02-14 15:25:19 +0800

    refacored mquiz homework

--- homework/01-mquiz/mquiz.go
+++ homework/01-mquiz/mquiz.go
@@ -41,48 +41,26 @@ func main() {
 	}
 
 	var input string
-	var index, score int
-	var usedProblems []int
+	var score int
+
+	// Shuffles the questions
+	rand.Seed(time.Now().Unix())
+	rand.Shuffle(len(rows), func(i, j int) {
+		rows[i], rows[j] = rows[j], rows[i]
+	})
 
 	// Prints each question and asks user for the answer
 	for ctr := 0; ctr < *questionsCount; ctr++ {
-		// Question randomizer
-		rand.Seed(time.Now().UnixNano())
-		index = rand.Intn(*questionsCount-0+1) + 0
+		fmt.Printf("Q: %s = ", rows[ctr][0])
+		fmt.Scan(&input)
 
-		if !findQuestion(usedProblems, index) {
-			// Question that was asked already is saved to a slice - checker to avoid repeating of questions
-			usedProblems = append(usedProblems, index)
-
-			fmt.Printf("Q: %s = ", rows[index][0])
-			fmt.Scan(&input)
-
-			// Checks if the database has the corresponding right answer to the question
-			if len(rows[index]) <= 1 {
-				log.Fatalf("No correct answer found. Please check your data.")
-			}
-
-			// Increments score if answer is correct
-			if input == rows[index][1] {
-				score++
-			}
-		} else {
-			ctr--
+		// Adds 1 to the score if user's answer is correct
+		if input == rows[ctr][1] {
+			score++
 		}
 	}
 
 	// Prints user's final score
 	println()
 	fmt.Println("You answered ", score, " out of ", *questionsCount, " questions correctly.")
-}
-
-// findQuestion checks whether a problem has been questioned
-func findQuestion(problems []int, index int) bool {
-	for _, i := range problems {
-		if i == index {
-			return true
-		}
-	}
-
-	return false
 }
